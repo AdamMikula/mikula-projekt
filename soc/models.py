725362b1cb41dd2a2d7677aa993072014a134bb6
Adam Mikula <adammikula27@gmail.com>
2024-05-31 11:50:04 +0200

    vytvorenie models

--- soc/models.py
+++ soc/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+class Trieda(models.Model):
+    nazov = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.nazov}" 
+    
+    class Meta:
+        verbose_name = "Trieda"
+        verbose_name_plural = "Triedy"
+        ordering = ["nazov"]
+
+
+class Ucitel(models.Model):
+    titul = models.CharField(max_length=20)
+    meno = models.CharField(max_length=20)
+    priezvisko = models.CharField(max_length=20)
+    email = models.EmailField(max_length = 254)
+    heslo = models.CharField(max_length=30)
+                                             
+
+    def __str__(self):
+        return f"{self.titul} {self.meno} {self.priezvisko} {self.email} {self.heslo}"
+    
+                
+class Student(models.Model):
+    meno = models.CharField(max_length=20)
+    priezvisko = models.CharField(max_length=20)
+    odbor = models.CharField(max_length=20)
+    trieda = models.ForeignKey(Trieda, on_delete=models.SET_NULL, null=True, blank=True)
+    email = models.EmailField(max_length = 254)
+    heslo = models.CharField(max_length=30)
+                                                                                         
+    def __str__(self):
+        return f"{self.meno} {self.priezvisko} {self.odbor} {self.trieda} {self.email} {self.heslo}"
+    
+    class Meta:
+        verbose_name = "Študent"
+        verbose_name_plural = "Študenti"
+        ordering = ["priezvisko"]
+
+class Tema(models.Model):
+    nazov = models.CharField(max_length=20)
+    popis = models.TextField(blank = True)
+    konzultant = models.ForeignKey(Ucitel, on_delete=models.SET_NULL, null=True, blank=True)
+    student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, blank=True)
+    CHOICES = (
+        ('VO', 'Voľne'),
+        ('CA', 'Čakajúce'),
+        ('OB', 'Obsadené'),
+    )
+    dostupnost = models.CharField(max_length=300, choices = CHOICES)
+
+    def __str__(self):
+        return f"{self.nazov} {self.popis} {self.konzultant} {self.student} {self.dostupnost}"
--- soc/views.py
+++ soc/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from . models import *
+import datetime
 
-# Create your views here.
+def vypis_skola(request):
+    studenti = Student.objects.all().order_by("priezvisko")
+    ucitelia = Ucitel.objects.all().order_by("priezvisko")
+    return render(request, "soc/index.html", {"studenti": studenti, "ucitelia": ucitelia})
+
